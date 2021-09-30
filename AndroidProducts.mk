111af2305808f20b1feed39d7776c494c88dcd6f
topes1 <banha218@gmail.com>
2021-09-30 15:22:11 +0100

    Rebrand for Spark OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/spark_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    spark_enchilada-user \
+    spark_enchilada-userdebug \
+    spark_enchilada-eng
